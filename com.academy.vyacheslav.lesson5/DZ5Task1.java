338738f9c4b5ac68a9fac08c75ba1b112727ffd5
RandomIzer2002 <73474962+RandomIzer2002@users.noreply.github.com>
2023-05-24 00:37:01 +0300

    Add files via upload

---
+++ com.academy.vyacheslav.lesson5/DZ5Task1.java
@@ -0,0 +1,7 @@
+public class DZ5Task1 {
+    public static void main(String[] args){
+        RanClass ranClass1 = new RanClass();
+		System.out.println("Стандартное число: " + ranClass1.getNumber());
+		System.out.println("Стандартный символ: " + ranClass1.getCharacter());
+    }
+}
---
+++ com.academy.vyacheslav.lesson5/DZ5Task3.java
@@ -0,0 +1,22 @@
+public class DZ5Task3 {
+    public static void main(String[] args){
+        Student student1 = new Student(1, "Ivanov Ivan Ivanovich", "Physics", 3, "P1", 1999);
+        Student student2 = new Student(2, "Petrov Petr Petrovich", "Chemistry", 2, "C2", 2000);
+        Student student3 = new Student(3, "Sidorov Sergei Sergeevich", "History", 1, "H3", 2001);
+        Student student4 = new Student(4, "Nikitin Nikolai Nikolaevich", "Math", 4, "M4", 1998);
+        Student student5 = new Student(5, "Popov Pavel Pavlovich", "Physics", 2, "P5", 2000);
+        Student student6 = new Student(6, "Babushkin Boris Borisovich", "Physics", 4, "P1", 1999);
+        Student student7 = new Student(7, "Rubtsov Roman Romanovich", "Chemistry", 3, "C2", 1999);
+        Student student8 = new Student(8, "Danilova Daria Vladimirovna", "History", 2, "H3", 2000);
+        Student student9 = new Student(9, "Kiseleva Kristina Olegovna", "Math", 1, "M4", 2001);
+        Student student10 = new Student(10, "Gorbacheva Galina Petrovna", "Physics", 5, "P5", 1998);
+
+        Student[] students = {student1, student2, student3, student4, student5, student6, student7, student8, student9, student10};
+
+        StudentManager.displayStudentsByCourse(students, 2);
+
+        System.out.println();
+
+        StudentManager.displayStudentsByBirthYear(students, 1999);
+    }
+}
