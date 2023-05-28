070afbe8baea055ddde3add1d28005d047b926c0
Valery Borys <valeryborys@gmail.com>
2023-05-28 23:20:17 +0200

    check3

--- com.academy.vyacheslav.lesson3/DZ3Task3.java
+++ com.academy.vyacheslav.lesson3/DZ3Task3.java
@@ -7,17 +7,17 @@ public class DZ3Task3 {
 		String passwordCreate = scanner.nextLine();
 		System.out.print("Введите пароль: ");
 		String password = scanner.nextLine();
-		int i = 0;
+		int i = 0; // эта переменная не нужна вобще
 		do {
 			i++;
-			if (password.equals(passwordCreate)) {
-			System.out.println("Пароль введён верно, добро пожаловать!");
+			if (password.equals(passwordCreate)) { // данное условие должно проверяться в операторе while(условие)
+			System.out.println("Пароль введён верно, добро пожаловать!"); // эта строчка должна быть после цикла do while
 		} else {
 			i = 0;
 			System.out.print("Неверный пароль! Пожалуйста, повторите попытку: ");
 			password = scanner.nextLine();
 		}
-	} while (i < 2);
+	} while (i < 2); //помести условие проверки равенства пароля сюда и тогда тебе не понадобится ни блок if ни переменная i
 		scanner.close();
 	}
 }
